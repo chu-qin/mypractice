f84816996582f0ea0e0a529a1467337062bb8cdd
初晴 <98371576+chu-qin@users.noreply.github.com>
2025-09-07 21:12:42 +0800

    Add files via upload
    瞅瞅有啥变化

--- C/Practice/test.c
+++ C/Practice/test.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main() {
     int a;
-    printf("Hello, World!\n");
+    printf("Please write a number.\n");
     scanf("%d", &a);
     printf("You input: a=%d\n", a);
     for (int i = 0; i < a; i++) {
         printf("i=%d\n", i);
     }
+    printf("a github test\n");
     return 0;
 }
